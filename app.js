9f8e8e218adb0e1e37218f4791a682fc5d468fc3
Wassim Assidi <assidi.wassim.dev@gmail.com>
2024-08-03 21:31:04 +0100

    update 57

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 const calculate = require('./calculate')
 
 app.get("/", (req, res) => {
-  res.send("Hello Devops - session 1 - 56 ");
+  res.send("Hello Devops - session 1 - 57 ");
 });
 
 app.post("/sum", (req, res) => {
